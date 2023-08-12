f852030c6003439fec0383892deebab28c364a43
Warisha-Rahat <116197266+Warisha-Rahat@users.noreply.github.com>
2023-08-13 04:56:18 +0530

    Update success.php

--- success.php
+++ success.php
@@ -1,8 +1,10 @@
 <!DOCTYPE html>
 <html> 
     <head><tittle></title></head>
-    <body><center><br><br>
+    <body>
+        <center><br><br>
         <h1> Thank you</h1><br>
-        <p> Your form has been submitted successfully</P></center>
+        <p> Your form has been submitted successfully</P>
+        </center>
 </body>
 </html>
